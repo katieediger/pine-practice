e8679ff69d44b19bbc47f6af92d3390de1ba8617
Katie Ediger <katieediger@gmail.com>
2018-01-17 15:27:38 -0600

    adding two files for chapter 5 exercises

---
+++ first_name.rb
@@ -0,0 +1,5 @@
+puts 'Hi, what is your first name?'
+first_name = gets.chomp
+puts 'Wow! ' + first_name + ' is a great first name! What is your last name?'
+last_name = gets.chomp
+puts 'Oh, so that means your full name is ' + first_name + ' ' + last_name + '.'
