5c61df86575f5df4dfa8235520c6923bf677cf6d
Katie Ediger <katieediger@gmail.com>
2018-01-17 16:53:22 -0600

    add files for chapter 7

--- chapter-7/comparison-methods.rb
+++ chapter-7/comparison-methods.rb
@@ -1,2 +1,12 @@
 puts 1 > 2
 puts 1 < 2
+puts ''
+puts 5 >= 5
+puts 5 <= 4
+puts ''
+puts 1 == 1
+puts 2 != 1
+puts ''
+puts 'cat' < 'dog'
+puts 'bug lady'      < 'Xander'
+puts 'bug lady'.downcase < 'Xander'.downcase
