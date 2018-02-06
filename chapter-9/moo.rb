bc0d6f9e1ab0378e6955aa5f119f0a14c302627b
Katie Ediger <katieediger@gmail.com>
2018-02-06 13:06:00 -0600

    add content to moo.rb and add note about error message

--- chapter-9/moo.rb
+++ chapter-9/moo.rb
@@ -1,7 +1,11 @@
 def say_moo number_of_moos
   puts 'mooooo...'*number_of_moos
+  'yellow submarine'
 end
-say_moo 3
-puts 'oink-oink'
+x = say_moo 3
+puts x.capitalize + ', dude...'
+puts x            + '.'
 say_moo
 say_moo
+# This program isn't working. Getting the below error message
+# moo.rb:1:in `say_moo': wrong number of arguments (0 for 1) (ArgumentError)
