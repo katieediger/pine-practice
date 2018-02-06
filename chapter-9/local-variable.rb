a4c84fbc1c734fe928f25411aaec3a13a505edcd
Katie Ediger <katieediger@gmail.com>
2018-02-06 08:47:22 -0600

    add local variable file

---
+++ chapter-9/local-variable.rb
@@ -0,0 +1,6 @@
+def double_this num
+  num_time_2 = num*2
+  puts num.to_s+' doubled is '+num_time_2.to_s
+end
+
+double_this 88
---
+++ chapter-9/moo.rb
@@ -0,0 +1,7 @@
+def say_moo number_of_moos
+  puts 'mooooo...'*number_of_moos
+end
+say_moo 3
+puts 'oink-oink'
+say_moo
+say_moo
