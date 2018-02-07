f2fe7bcb2c0faf0dabaa918016dee2a903873cf8
Katie Ediger <katieediger@gmail.com>
2018-02-07 14:23:10 -0600

    updated file for chapter 9 so that it now works.

--- chapter-9/moo.rb
+++ chapter-9/moo.rb
@@ -5,7 +5,3 @@ end
 x = say_moo 3
 puts x.capitalize + ', dude...'
 puts x            + '.'
-say_moo
-say_moo
-# This program isn't working. Getting the below error message
-# moo.rb:1:in `say_moo': wrong number of arguments (0 for 1) (ArgumentError)
